ed4521999c0cbd25f62950bb1dc0d44111ee9bf4
Chengis Khan <mcheng3@nd.edu>
2020-06-22 17:36:52 -0400

    new info

---
+++ HTML Website/js/locations.js
@@ -0,0 +1,24 @@
+locations ({
+   "events": [
+       {
+        "location": "Alvor, Portugal",
+        "date": "July 2019",
+        "map": "map/Alvor.jpg"
+       },
+       {
+        "location": "Ballymaloe, Ireland",
+        "date": "March 2018",
+        "map": "map/Ballymaloe.png"
+       },
+       {
+        "location": "Dublin, Ireland",
+        "date": "December 2017",
+        "map": "map/dublin.png"
+       },
+       {
+        "location": "Paris, France",
+        "date": "July 2017",
+        "map": "map/paris.jpg"
+       }
+   ] 
+});
