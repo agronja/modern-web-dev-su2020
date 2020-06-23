76bd8927cffbf136e8584d8da42437c6bd97c7b9
Josh Agron <jagron907@gmail.com>
2020-06-22 22:20:58 -0800

    Changed featured photo to maps

--- HTML Website/js/locations.js
+++ HTML Website/js/locations.js
@@ -3,22 +3,22 @@ locations ({
        {
         "location": "Alvor, Portugal",
         "date": "July 2019",
-        "map": "map/Alvor.jpg"
+        "map": "js/map/Alvor.jpg"
        },
        {
         "location": "Ballymaloe, Ireland",
         "date": "March 2018",
-        "map": "map/Ballymaloe.png"
+        "map": "js/map/Ballymaloe.png"
        },
        {
         "location": "Dublin, Ireland",
         "date": "December 2017",
-        "map": "map/dublin.png"
+        "map": "js/map/dublin.png"
        },
        {
         "location": "Paris, France",
         "date": "July 2017",
-        "map": "map/paris.jpg"
+        "map": "js/map/paris.jpg"
        }
-   ] 
+   ]
 });
--- HTML Website/js/showLocations.js
+++ HTML Website/js/showLocations.js
@@ -1,15 +1,28 @@
 function locations(data) {
     var newContent = '';
-    
+
     for (var i = 0; i < data.events.length; i++) {
-        newContent += '<div class="event>';
+        newContent += '<figure class="grid-item-' + i + '">';
         newContent += '<img src="' + data.events[i].map + '"';
-        newContent += ' alt="' + data.events[i].location + '" />';
+        newContent += ' alt="' + data.events[i].location + '"';
+        newContent += ' class="map_img" />';
         newContent += '<p><b>' + data.events[i].location + '</b><br>';
         newContent += data.events[i].date + '</p>';
-        newContent += '</div>';
+        newContent += '</figure>';
     }
-    
-    document.getElementById('content'). innerHtml.newContent;
-    
+
+    document.getElementById('map').innerHTML += newContent;
+
+    var column = 0;
+    for (var i = 0; i < data.events.length; i++) {
+      var dataID = 'grid-item-' + i;
+      document.getElementById(dataID).style.gridColumnStart = column;
+      column++;
+      document.getElementById(dataID).style.gridColumnEnd = column;
+      document.getElementById(dataID).style.margin = auto;
+
+    }
+
+
+
 }
