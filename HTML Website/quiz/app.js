4eeeb8366a297344fc349e6cbbb988d07b0c4811
Chengis Khan <mcheng3@nd.edu>
2020-06-24 18:14:13 -0400

    more

---
+++ HTML Website/quiz/app.js
@@ -0,0 +1,81 @@
+angular.module('app', ['ngMaterial', 'ngMessages']);
+
+/*--------------------- Home Component ---------------------*/
+const home = {
+    templateUrl: './home/home.html',
+    controller: 'HomeController'
+}
+
+// Home Component with Routing (Routed / Stateful)
+angular.module('app').component('home', home);
+
+// Home Controller with dependency injection using the array method
+angular.module('app').controller('HomeController', ['ExampleService', function (ExampleService) {
+    
+    
+
+}]);
+/*--------------------- Home Component ---------------------*/
+
+/*--------------------- Settings Component ---------------------*/
+const settings = {
+    templateUrl: '',
+    controller: 'SettingsController'
+}
+
+// Settings Component with Routing (Routed / Stateful)
+angular.module('app').component('settings', settings)
+
+// Settings Controller with dependency injection using $inject method
+function SettingsController(ExampleService) {
+
+}
+SettingsController.$inject = ['ExampleService'];
+angular.module('app').controller('SettingsController', SettingsController);
+/*--------------------- Settings Component ---------------------*/
+
+/*--------------------- Example Service ---------------------*/
+function ExampleService() {
+    // Services are Singletons
+    $http
+    // Properties
+    // Methods
+}
+angular.module('app').controller('SecondController',['question', function(questions) {
+    const ctrl = this;
+    ctrl.questions [ 
+        {
+            'image': "images/Port/Snapseed.jpg",
+			'question': "Question 1: What place does this image come from?",
+			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'answer': 2
+		},
+		{
+            'image': "" ,
+			'question': "Question 2: What place does this image come from?",
+			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'answer': 2
+		},
+		{
+            'image': "",
+			'question': "Question 3: What place does this image come from?",
+			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'answer': 2
+		},
+		{
+            "image": "",
+			'question': "Question 4: What place does this image come from? ",
+			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'answer': 2
+		},
+		{	
+            'image': "",
+			'question': "Question 5: What place does this image come from?",
+			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'answer': 2
+		}                      
+    ]
+                     
+                                 }]);
+/*--------------------- Example Service ---------------------*/
+
---
+++ HTML Website/quiz/data.js
@@ -0,0 +1,34 @@
+{
+    'questions': [
+    {
+            'image': "images/Port/Snapseed.jpg",
+			'question': "Question 1: What place does this image come from?",
+			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'answer': 2
+		},
+		{
+            'image': "" ,
+			'question': "Question 2: What place does this image come from?",
+			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'answer': 2
+		},
+		{
+            'image': "",
+			'question': "Question 3: What place does this image come from?",
+			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'answer': 2
+		},
+		{
+            "image": "",
+			'question': "Question 4: What place does this image come from? ",
+			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'answer': 2
+		},
+		{	
+            'image': "",
+			'question': "Question 5: What place does this image come from?",
+			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'answer': 2
+		}                      
+    ]
+}
