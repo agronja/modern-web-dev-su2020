33d3e1cdda427daf2bb715fbc57a3f6e4b48c785
Chengis Khan <mcheng3@nd.edu>
2020-06-24 18:56:14 -0400

    more

--- HTML Website/quiz/data.js
+++ HTML Website/quiz/data.js
@@ -3,31 +3,31 @@
     {
             'image': "images/Port/Snapseed.jpg",
 			'question': "Question 1: What place does this image come from?",
-			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
-			'answer': 2
+			'options': ["Portugal", "Spain", "Ireland", "France"],
+			'answer': 0
 		},
 		{
-            'image': "" ,
+            'image': "images/Ire/IKEF6155.jpg" ,
 			'question': "Question 2: What place does this image come from?",
-			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
-			'answer': 2
+			'options': ["Berlin", "Portugal", "France", "Ireland"],
+			'answer': 3
 		},
 		{
-            'image': "",
+            'image': "images/Fra/QSXA1624 (1).jpg",
 			'question': "Question 3: What place does this image come from?",
-			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
-			'answer': 2
+			'options': ["Italy", "France", "Ireland", "Portugal"],
+			'answer': 1
 		},
 		{
-            "image": "",
+            "image": "images/Ire/CQJN2928.jpg",
 			'question': "Question 4: What place does this image come from? ",
-			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
-			'answer': 2
+			'options': ["Ireland", "Portugal", "Spain", "France"],
+			'answer': 0
 		},
 		{	
-            'image': "",
+            'image': "images/Port/85D46348-A1D2-44D3-9C2F-FA6DB64DB942.jpg",
 			'question': "Question 5: What place does this image come from?",
-			'options': ["Alvor", "Paris", "Ballymaloe", "Dublin"],
+			'options': ["Ireland", "India", "Portugl", "France"],
 			'answer': 2
 		}                      
     ]
