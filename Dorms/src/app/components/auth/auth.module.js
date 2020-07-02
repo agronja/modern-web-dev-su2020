0045fe3abc9276adafeafff6b90dd0323f79b235
Chengis Khan <mcheng3@nd.edu>
2020-07-02 18:00:11 -0400

    more

--- Dorms/src/app/components/auth/auth.module.js
+++ Dorms/src/app/components/auth/auth.module.js
@@ -6,10 +6,10 @@ angular
     'ngMaterial',
     'ngMessages'
 ])
-    .config(function ($locationProvider, $mdThemingProvider, ParseProvider){
+    .config(function (ParseProvider){
     
-        var MY_PARSE_APP_ID = 'TjuZFnHAn9J5Fn68Qx15iEIrpAArmKSuAHvwMpMo';
-        var MY_PARSE_JS_KEY = 'IWFF1v6ucEEnBPp6965PbD4WaVCe65vDV1VZZRbk';
+        var MY_PARSE_APP_ID = '';
+        var MY_PARSE_JS_KEY = '';
         ParseProvider.initialize(MY_PARSE_APP_ID, MY_PARSE_JS_KEY);
         ParseProvider.serverURL = 'https://parseapi.back4app.com/';
 })
