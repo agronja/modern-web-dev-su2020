2e75b7b9202ff323eff0b9b763f87abb23969376
Chengis Khan <mcheng3@nd.edu>
2020-07-02 17:40:16 -0400

    more

--- Dorms/src/app/common/app.run.js
+++ Dorms/src/app/common/app.run.js
@@ -2,7 +2,37 @@ function init($rootScope, $uiRouter, $mdSidenav, $timeout, $log) {
     var Visualizer = window['ui-router-visualizer'].Visualizer;
     var pluginInstance = $uiRouter.plugin(Visualizer);
     
+    $rootScope.toggleMenu = toggleMenu;
+    $rootScope.isEmpty = isEmpty;
     
+    function debounce(callback, interval) {
+        var timeout = null;
+        return function () {
+            $timeout.cancel(timeout);
+            timeout = $timeout(function () {
+                callback.apply(this);
+            }, interval);
+        };
+    }
+    
+    function toggleMenu(navId) {
+        return debounce(function () {
+            $mdSidenav(navId)
+                .toggle()
+                .then(function () {
+                    $log.log("toggle " + navId + " is done");
+            });
+        }, 0);
+    }
+    
+    function isEmpty(obj) {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key))
+                return false;
+            else   
+                return true;
+        }
+    }
     
     
 }
