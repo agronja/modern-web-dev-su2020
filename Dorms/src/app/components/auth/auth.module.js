f790fe14e16666e6f55c10cacabd324d3fd4cf4b
Chengis Khan <mcheng3@nd.edu>
2020-07-02 19:07:25 -0400

    more

--- Dorms/src/app/components/auth/auth.module.js
+++ Dorms/src/app/components/auth/auth.module.js
@@ -1,6 +1,7 @@
 angular
     .module('components.auth', [
     'ui.router',
+    'firebase',
     'ngParse',
     'ui.router',
     'ngMaterial',
@@ -12,4 +13,4 @@ angular
         var MY_PARSE_JS_KEY = '';
         ParseProvider.initialize(MY_PARSE_APP_ID, MY_PARSE_JS_KEY);
         ParseProvider.serverURL = 'https://parseapi.back4app.com/';
-})
+});
