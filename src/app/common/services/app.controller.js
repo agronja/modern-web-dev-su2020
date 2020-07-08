6718a982740a38f32497a888c277a161aea94d7d
Chengis Khan <mcheng3@nd.edu>
2020-07-08 17:39:28 -0400

    more

--- src/app/common/services/app.controller.js
+++ src/app/common/services/app.controller.js
@@ -2,6 +2,20 @@ function AppController($state) {
   var ctrl = this;
 }
 
+New(obj) {
+    if (angular.isUndefined(obj)) {
+        const parseObject = new this.Parse.Object(this.name)
+        this.Parse.defineAttributes(parseObject, this.fields)
+        return parseObject
+    }
+    else {
+        this.Parse.defineAttributes(obj, this.fields)
+        return obj
+    }
+}
+
+
+
 angular
   .module('common')
   .controller('AppController', AppController)
