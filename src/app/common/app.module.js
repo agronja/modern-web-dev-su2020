90cdb08bd62c31ea3f160083be14c872bc5bee50
Josh Agron <jagron907@gmail.com>
2020-07-07 18:48:30 -0800

    added id and key

--- src/app/common/app.module.js
+++ src/app/common/app.module.js
@@ -5,8 +5,8 @@ angular
   ])
 
     .config(function($locationProvider, ParseProvider) {
-        var MY_PARSE_APP_ID = '';
-        var MY_PARSE_JS_KEY = '';
+        var MY_PARSE_APP_ID = 'jHqsqPKdcgNS2uolToydrK8ZrDvAUTCjSdlDKDNb';
+        var MY_PARSE_JS_KEY = 'DsXsf8ASScwqMVEuNxtKfoPNfGXhgTuAbpPpC4X0';
         ParseProvider.initialize(MY_PARSE_APP_ID, MY_PARSE_JS_KEY);
         ParseProvider.serverURL = 'https://...back4app.com/';
 });
