e49210ad9660269bf4270630265c9789b37f657c
Chengis Khan <mcheng3@nd.edu>
2020-07-08 10:50:40 -0400

    more

--- src/app/components/dormEvents/comment.js
+++ src/app/components/dormEvents/comment.js
@@ -1,4 +1,6 @@
-//how to link objects
+//this will be the rating system for dorm events
+//the user will enter their comment in a textbox in the HTML 
+//and then that comment will be stored on the server
 Parse.initialize("myAppID");
 Parse.serverURL = "https://";
 
@@ -7,28 +9,29 @@ var Post = Parse.Object.extend("Post");
 var post = new Post();
 
 // NEEDS TO BE CHANGED: take data from user input
-var data = {
-    "body": "Hi", 
-    "tags": ["first-post", "welcome"],
-    "numComments": 0,
-    "author": "Mark"  
-},
+var id = angular.element(document.getElementById("textbox"));
+//var data = {
+//    "body": "Hi", 
+//    "tags": ["first-post", "welcome"],
+//    "numComments": 0,
+//    "author": "Mark"  
+//},
 
-post.save(data, {
+post.save(id, {
     success:function(obj) {
         console.log("Successfully saved " + obj.id);
         
-        var Comment = Parse.Object.extend("Comment");
-        var comment = new Comment();
-        comment.set("message", "Thanks for your vote");
-        comment.set("parent", post);
+        var Events = Parse.Object.extend("Events");
+        var event = new Events();
+        event.set("message", "Thanks for your vote");
+        event.set("parent", post);
         
-        comment.save(null, {
+        event.save(null, {
             success: function(obj) {
                 console.log("Saved comment " + obj.id);
                 
-                var comments = post.relation("comments");
-                comments.add(comment);
+                var events = post.relation("events");
+                events.add(event);
                 post.save();
             }
         });
--- src/app/dorms-parse.js
+++ src/app/dorms-parse.js
@@ -2,14 +2,42 @@
 Parse.initialize("myAppID");
 Parse.serverURL = 'https://';
 
-var id = angular.element('');
+// objects for the list of dorms
 var Dorms = Parse.Object.extend("Dorms");
 var Dorm = Parse.Object.extend("Dorm");
 
-
+//go through each dorm 
 var q = new Parse.Query("Dorm");
-q.equalTo("name", "id");
-q.find().then(function(results) {
-    console.log("## EQUALTO ##");
-    console.log(results);
+q.find().then(function(specificDorm) {
+    for (var i = 0; i < specifcDorm.length; i++){
+        var dormName = specifcDorm[i];
+        //get data corresponding to the dormName
+        var dormsTemplate = {
+	       template: `
+		      <div> 
+                <h3> dormName.name</h3>
+                <p>"Founded in the year" + dormName.date</p>
+                <p>"Capacity of " + dormName.capacity</p>
+                <p>"Their mascot is the " dormName.mascot</p>
+                <p>dormName.location</p>
+              </div>
+            `
+        };
+        //send data to dorms component which will render the information
+        angular
+	       .module('dorms')
+	       .component('dormsTemplate', dormsTemplate)
+	       .config(function ($stateProvider) {
+		      $stateProvider
+			     .state('dorms.template', {
+				    url: '/template',
+				    views: {
+					   'details@dorms': {
+						  component: 'dormsTemplate'
+				        }
+				    }
+			     });
+	       }); 
+    //repeat cycle
+    }
 });
