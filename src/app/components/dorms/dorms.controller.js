3ba7222fb11a81f7acd400529f2d5f015e8871ff
Josh Agron <jagron907@gmail.com>
2020-07-08 18:23:18 -0800

    fuuuuuuuck

--- src/app/components/dorms/dorms.controller.js
+++ src/app/components/dorms/dorms.controller.js
@@ -1,21 +1,19 @@
 function DormsController($state) {
     const ctrl = this;
-<<<<<<< HEAD
 
-    ctrl.getAll = $state.getAll;
-    console.log(ctrl.getAll);
-=======
-    /*
-    DormsService.getAllDorms().then(function(dorm_data) {
-        ctrl.dorm_data = dorm_data;
-<<<<<<< HEAD
-        
-    });
-=======
-        ctrl.dorms = ctrl.dorm_data.data;
-    }); */
->>>>>>> 76d6c7d2f23065fdc7b7d4b380846d82632851aa
->>>>>>> 207dcba5f34967df5c3ce3c962f446515dbe21b2
+    ctrl.$onInit = function(){
+      console.log("found the following dorm: ", ctrl.dorms)
+
+      ctrl.searchDorms = function() {
+        ctrl.list = new Array()
+
+        for(var i = 0; i < ctrl.dorms.length; i++){
+          if(ctrl.dorms[i].get('Name') == ctrl.name){
+            ctrl.list.push(ctrl.textbooks[i])
+          }
+        }
+      }
+    }
 }
 
 angular
