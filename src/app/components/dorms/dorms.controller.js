09cb3b6cff75d63695104e3d558becaaa81892aa
Chengis Khan <mcheng3@nd.edu>
2020-07-08 20:42:51 -0400

    more

--- src/app/components/dorms/dorms.controller.js
+++ src/app/components/dorms/dorms.controller.js
@@ -1,9 +1,9 @@
 function DormsController() {
     const ctrl = this;
     
-    DormsService.getAllDorms().then(function(dorm_data) {
+    dormsService.getAll().then(function(dorm_data) {
         ctrl.dorm_data = dorm_data;
-        ctrl.dorms = ctrl.dorm_data.data;
+        ctrl.dorms_info = ctrl.dorm_data.data;
     });
 }
 
