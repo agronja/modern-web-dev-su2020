5787e073a6018e65bbae81ea49cf635673c2ca1c
Chengis Khan <mcheng3@nd.edu>
2020-07-13 17:39:04 -0400

    more

--- src/app/common/auth/auth.module.js
+++ src/app/common/auth/auth.module.js
@@ -1,31 +1,9 @@
 angular
   .module('components.auth', [
     'ui.router',
-    'firebase'
+    'ngParse'
   ])
-  .config(function ($firebaseRefProvider) {
-
-    var config = {
-      //apiKey: "AIzaSyCsNISt3dFx7dy5AImIIk62jDDd0OLvZK0",
-      //authDomain: "contacts-manager-e486f.firebaseapp.com",
-      //databaseURL: "https://contacts-manager-e486f.firebaseio.com",
-      //storageBucket: "contacts-manager-e486f.appspot.com",
-
-      var MY_PARSE_APP_ID = 'Xm5mvESVMpYL3fhQVoY3CufO9iRrWsVhl8Qr1RYp';
-      var MY_PARSE_JS_KEY = 'ivH9AWT51N75391o5tgHv8gEhWiVEXbl82WeGqyX';
-      ParseProvider.initialize(MY_PARSE_APP_ID, MY_PARSE_JS_KEY);
-      ParseProvider.serverURL = 'https://parseapi.back4app.com/';
-    };
-
-    $firebaseRefProvider
-      .registerUrl({
-        default: config.databaseURL,
-        //changed
-        events: config.databaseURL + '/events'
-      });
-
-    firebase.initializeApp(config);
-  })
+  
   .run(function ($transitions, $state, AuthService) {
     $transitions.onStart({
       to: function (state) {
