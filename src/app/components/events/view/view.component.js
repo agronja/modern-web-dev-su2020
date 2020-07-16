e7a3befb3d7f1f416f2ca284cd8299e5ad91b106
Josh Agron <jagron907@gmail.com>
2020-07-16 12:16:12 -0800

    fixing your mistake

--- src/app/components/events/view/view.component.js
+++ src/app/components/events/view/view.component.js
@@ -1,25 +1,25 @@
 //component for the events tab
 var view = {
-    templateUrl: './view.html',
-    controller: 'ViewController',
-    bindings: {
-      events: '<'
-    }
+  templateUrl: './view.html',
+  controller: 'ViewController',
+  bindings: {
+    events: '<'
+  }
 };
 
 angular
-    .module('view')
-    .component('view', view)
-    .config(function ($stateProvider) {
-        $stateProvider
-            .state('view', {
-                parent: 'events',
-                url: '/view',
-                component: 'view',
-                resolve: {
-                  events: function(eventsService) {
-                    return eventsService.getAll()
-                  }
-                }
-        })
-});
+  .module('view')
+  .component('view', view)
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('view', {
+        parent: 'events',
+        url: '/view',
+        component: 'view',
+        resolve: {
+          events: function(eventsService) {
+            return eventsService.getAll()
+          }
+        }
+      })
+  });
