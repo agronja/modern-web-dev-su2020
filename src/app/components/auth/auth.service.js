58d4825e6630377bd7129f546849f326056e3faa
Josh Agron <jagron907@gmail.com>
2020-07-15 18:46:05 -0800

    Moved Auth to components

--- src/app/components/auth/auth.service.js
+++ src/app/components/auth/auth.service.js
@@ -1,47 +1,47 @@
 function authService(Parse) {
-/*
-  constructor(Parse){
-    this.Parse = Parse
-    this.collection = []
-    this.authData = {}
-    this.name = 'User'
-    this.fields = [
-      'username',
-      'email',
-      'password',
-      'Dorm'
-    ]
-  }
-*/
+  /*
+    constructor(Parse){
+      this.Parse = Parse
+      this.collection = []
+      this.authData = {}
+      this.name = 'User'
+      this.fields = [
+        'username',
+        'email',
+        'password',
+        'Dorm'
+      ]
+    }
+  */
   var user = new Parse.User();
 
-/* Can this be changed to signUp with the setEmail, setPassword, etc.?
-  New(obj) {
-    if(angular.isUndefined(obj)){
-      const parseObject = new this.Parse.Object(this.name)
-      this.Parse.defineAttributes(parseObject, this.fields)
-      return parseObject
-    } else {
-      this.Parse.defineAttributes(obj, this.fields)
-      return obj
+  /* Can this be changed to signUp with the setEmail, setPassword, etc.?
+    New(obj) {
+      if(angular.isUndefined(obj)){
+        const parseObject = new this.Parse.Object(this.name)
+        this.Parse.defineAttributes(parseObject, this.fields)
+        return parseObject
+      } else {
+        this.Parse.defineAttributes(obj, this.fields)
+        return obj
+      }
     }
-  }
 
-  this.onSignIn = function (user) {
-    return user
-      .logIn("myname", "mypass");
-  };
-*/
-  this.login = function (user) {
+    this.onSignIn = function (user) {
+      return user
+        .logIn("myname", "mypass");
+    };
+  */
+  this.login = function(user) {
     return user
       .logIn(user.email, user.password)
   };
 
-  this.register = function (user) {
+  this.register = function(user) {
     return user
       .signUp(user.email, user.password, user.username, user.Dorm)
   };
-  this.logout = function () {
+  this.logout = function() {
     return user
       .logOut()
   };
@@ -50,12 +50,17 @@ function authService(Parse) {
     return user
       .$waitForSignIn().then(onSignIn);
   }; */
-  this.isAuthenticated = function () {
+  this.isAuthenticated = function() {
     return user.authenticated();
   };
-  this.getUser = function () {
-    console.log(Parse.User.current())
-    return Parse.User.current();
+  this.getUser = function() {
+    var currentUser = user.current();
+    if (currentUser) {
+      return currentUser
+    } else {
+      return user
+        .signUp(user.email, user.password, user.username, user.dorm)
+    }
   };
 
 }
